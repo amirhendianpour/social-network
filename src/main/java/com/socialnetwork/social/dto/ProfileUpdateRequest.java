172b7f4d09dee6e4f52a63235b7106fb0540186c
Amir <you@example.com>
2026-09-01 00:48:04 +0330

    add edit username

--- src/main/java/com/socialnetwork/social/dto/ProfileUpdateRequest.java
+++ src/main/java/com/socialnetwork/social/dto/ProfileUpdateRequest.java
@@ -1,5 +1,10 @@
 package com.socialnetwork.social.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ProfileUpdateRequest {
     private String firstName;
     private String lastName;
@@ -8,20 +13,4 @@ public class ProfileUpdateRequest {
     private String phoneNumber;
     private String username;
 
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-
-    public String getBio() { return bio; }
-    public void setBio(String bio) { this.bio = bio; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
 }
--- src/main/java/com/socialnetwork/social/service/ProfileService.java
+++ src/main/java/com/socialnetwork/social/service/ProfileService.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Instant;
 import java.util.UUID;
 
 @Service
