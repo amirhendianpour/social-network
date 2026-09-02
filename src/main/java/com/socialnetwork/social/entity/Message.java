bbee022030cbb446cb20baba10365fa817106033
Amir <you@example.com>
2026-09-02 16:19:25 +0330

    update forward message 02

--- src/main/java/com/socialnetwork/social/entity/Message.java
+++ src/main/java/com/socialnetwork/social/entity/Message.java
@@ -30,6 +30,7 @@ public class Message {
 
     private String replyToId;
     private String mediaKey;
+    @Column(nullable = false, columnDefinition = "boolean default false")
     private boolean isForwarded = false;
 
     private Instant timestamp;
